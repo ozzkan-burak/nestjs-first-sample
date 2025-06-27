582de78d7e017e2ea3bee8aeeac1b2817f5d72bc
ozzkan-burak <brk.ozzkan@gmail.com>
2025-06-27 23:44:34 +0300

    episode2

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!---';
   }
+  create(body: any): string {
+    // Assuming the body contains a 'message' property
+    return `Received message: ${JSON.stringify(body)}`;
+  }
 }
