3c9d9f9faa12081547de8aa694287e1cf4031cc4
ozzkan-burak <brk.ozzkan@gmail.com>
2025-07-12 16:24:19 +0300

    episode10

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid'; // Ensure you have uuid installed
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -22,5 +23,16 @@ export class UserService {
     await this.userRepository.save(newUser);
     //const result = await this.userRepository.save(user);
     return { message: 'Saved User', newUser };
+  }
+
+  async updateUser(updateUserDto: UpdateUserDto) {
+    const user = await this.userRepository.findOneBy({ id: updateUserDto.id });
+    if (!user) {
+      return { message: 'User not found' };
+    }
+    user.name = updateUserDto.name.toLowerCase();
+    user.email = updateUserDto.email.toLowerCase();
+    await this.userRepository.save(user);
+    return { message: 'Updated User', user };
   }
 }
