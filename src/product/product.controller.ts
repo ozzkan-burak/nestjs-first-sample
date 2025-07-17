bc674c775e2cc7879a8816c6f856e9f4783b8629
ozzkan-burak <brk.ozzkan@gmail.com>
2025-07-17 21:22:16 +0300

    episode15

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
 @ApiTags('product')
 export class ProductController {
+  constructor(private readonly productService: ProductService) {}
   @Get()
   @ApiOperation({
     summary: 'Active Products',
@@ -11,5 +14,10 @@ export class ProductController {
   getActiveProducts() {
     // Logic to retrieve active products
     return [];
+  }
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productService.createProduct(createProductDto);
   }
 }
